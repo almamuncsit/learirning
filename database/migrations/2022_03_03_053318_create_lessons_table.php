a50044b9b634e0f51dc2225ceaaf68f35db29b2f
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2022-03-03 11:40:53 +0600

    Add database migrations

--- database/migrations/2022_03_03_053318_create_lessons_table.php
+++ database/migrations/2022_03_03_053318_create_lessons_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('lessons', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('video_url', 150)->nullable();
+            $table->foreignId('courese_id');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lessons');
     }
 };
