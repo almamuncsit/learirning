842410df6e44c25db0409912bf7cd8a7b0bb4533
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2022-03-03 11:42:59 +0600

    Make title nullable

--- database/migrations/2022_03_03_053318_create_lessons_table.php
+++ database/migrations/2022_03_03_053318_create_lessons_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->string('video_url', 150)->nullable();
             $table->foreignId('courese_id');
